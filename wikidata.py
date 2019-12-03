534caf03ef53849170c4dc14a02b712771ae61ad
soarthec <soarthec@macbook-air-de-benoit.home>
2019-12-03 11:29:47 +0100

    version

--- wikidata.py
+++ wikidata.py
@@ -1,9 +1,14 @@
-WIKIDATA_REQUEST1 ="""SELECT DISTINCT ?item ?itemLabel ?image ?value ?valueLabel
+WIKIDATA_REQUEST1 ="""SELECT DISTINCT ?item ?itemLabel ?nomscientifique ?rangtaxinomique ?taxonsuperieur ?identifiantSPECIES ?image ?value ?valueLabel
 WHERE
 {
   ?item wdt:P2040 ?value;
         rdfs:label ?itemLabel .
-  OPTIONAL { ?item wdt:P18 ?image. }      
+  OPTIONAL { ?item wdt:P18 ?image. }   
+  OPTIONAL { ?item wdt:P225 ?nomscientifique. } 
+  OPTIONAL { ?item wdt:P105 ?rangtaxinomique. }   
+  OPTIONAL { ?item wdt:P171 ?taxonsuperieur. }   
+  OPTIONAL { ?item wdt:P2040 ?identifiantSPECIES. } 
+  
   FILTER(CONTAINS(LCASE(?itemLabel), """
 
 WIKIDATA_REQUEST2 = """))
