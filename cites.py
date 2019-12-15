28290d23abf225cfa0db3ac73727aad5d0f8f5fe
soarthec <soarthec@macbook-air-de-benoit.home>
2019-12-15 14:27:02 +0100

    last

--- cites.py
+++ cites.py
@@ -1,4 +1,4 @@
 CITES1 = "https://api.speciesplus.net/api/v1/taxon_concepts/"
 CITES2_FR = "/distributions.json"
 CITES2 = "/distributions.json?language=fr"
-CITES_KEY = "re7ck8hwhPpbTDXEhjHpuAtt"
+CITES_KEY = ""
